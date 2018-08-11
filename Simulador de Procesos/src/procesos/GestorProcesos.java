8f0882e04c71d049f257a885eb0f187ee0ca7ac6
Lesteraft <40004561+Lesteraft@users.noreply.github.com>
2018-08-10 23:45:13 -0600

    v5.2

--- Simulador de Procesos/src/procesos/GestorProcesos.java
+++ Simulador de Procesos/src/procesos/GestorProcesos.java
@@ -28,11 +28,6 @@ public class GestorProcesos {
 
 			System.out.println("CicloDelProcesador #"+n);
 			
-			gestor.procesoNuevo();
-			
-			gestor.estadoNuevo_Listo();
-			
-			gestor.estadoListo_Ejecucion();
 			
 			gestor.cicloEjecucion();
 			
